c36d314b5f8870c5407350c79e9a98261646b532
Ihor04 <g06477544@gmail.com>
2024-10-28 19:11:45 +0200

    Third commit

--- Task_2.2.py
+++ Task_2.2.py
@@ -5,5 +5,5 @@ num_4 = (number % 100) // 10
 num_3 = (number % 1000) // 100
 num_2 = (number % 10000) // 1000
 num_1 = (number % 100000) // 10000
-print(num_5, num_4, num_3, num_2, num_1, sep="")
-
+output = ((num_5 * 10000) + (num_4 * 1000) + (num_3 * 100) + (num_2 * 10) + num_1)
+print(output)
